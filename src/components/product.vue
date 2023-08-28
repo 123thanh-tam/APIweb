ac9e88fa7a665fbb2b109b55fec1171bc8ab6282
thanhtam <vutoan101102@gmail.com>
2023-08-28 22:01:04 +0700

    Add new project

---
+++ src/components/product.vue
@@ -0,0 +1,23 @@
+<template>
+		<div>
+			<div v-for="pro in products" :key="pro.id" class="job">
+				<h1>{{pro}}</h1>
+			</div>
+		</div>
+</template>
+<script>
+
+export default ({
+	data(){
+		return{
+			products: []
+		}
+	},
+	mounted(){
+		fetch('http://localhost:3000/products')
+			.then(res => res.json())
+			.then(data => this.products = data)
+			.catch(err => console.log(err.message))
+	}
+})
+</script>
